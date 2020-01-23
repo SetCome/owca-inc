5b3f71697d2301d64bc0d7f59934678fdb3e2421
SetCome <45269359+SetCome@users.noreply.github.com>
2020-01-23 19:46:15 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const data = require('./dane');
 let owce = data.getOwce();
 console.log(owce);
 
-app.get('/', function (req, res) {
-  res.send('owca taboret')
-})
 
 app.get('/owce', function (req, res) {
   res.json(owce);
@@ -27,22 +24,27 @@ app.post('/owce', function (req,res){
   console.log("dostaliśmy owcę:", owca);
   owce.push(owca);
   res.json(owca);
+  data.save();
 })
 
 app.delete('/owce/:ID', function (req, res) {
    const ID = req.params.ID;
    owce = owce.filter( owca => owca.ID != ID);
    res.send('ojej! usuwamy owcę ' + ID)
+   data.save();
 })
 
 // dodać edycję owiec 
 app.patch('/owce/:ID', function (req,res){
   const owca = owce.find(owca => owca.ID == req.params.ID)
-  // owca do edycji - owca
-  // wszystkie parametry które są w req.body mają nadpisać owcę
+  if(typeof req.body.Age !== "undefined") owca.Age = req.body.Age;
+  if(typeof req.body.location !== "undefined") owca.location = req.body.location;
+  if(typeof req.body.needsShaving !== "undefined") owca.needsShaving = req.body.needsShaving;
+  if(typeof req.body.broken !== "undefined") owca.broken = req.body.broken;
   res.json(owca);
+  data.save();
 })
 
-
+app.use(express.static('static'))
 
 app.listen(8080, () => console.log(`Example app listening on port ${8080}!`))
